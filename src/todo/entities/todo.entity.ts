5abccb979049e2557ad8e9db745eec176e5eab0b
Tolu Ajia <toluajia@gmail.com>
2023-06-08 17:03:47 +0100

    chore: define the Todo entity with field descriptions

--- src/todo/entities/todo.entity.ts
+++ src/todo/entities/todo.entity.ts
@@ -2,6 +2,15 @@ import { ObjectType, Field, Int } from '@nestjs/graphql';
 
 @ObjectType()
 export class Todo {
-  @Field(() => Int, { description: 'Example field (placeholder)' })
-  exampleField: number;
+  @Field(() => Int, { description: 'The unique identifier of a task/to-do.' })
+  id: number;
+
+  @Field({ description: 'The name of a task/to-do.' })
+  title: string;
+
+  @Field({ description: 'More details on a task/to-do', nullable: true })
+  description?: string;
+
+  @Field({ description: 'The status of a task/to-do', defaultValue: false })
+  completed: boolean;
 }
