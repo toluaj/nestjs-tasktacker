1c01e38c21634f4afb2c15067c86b2ab66f1c690
Tolu Ajia <toluajia@gmail.com>
2023-06-10 22:29:02 +0100

    chore: add user model and define user/todo relationship

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,15 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
+import * as passport from 'passport';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.useGlobalPipes(new ValidationPipe());
+
+  app.use(passport.initialize());
+
   await app.listen(3000);
 }
 bootstrap();
